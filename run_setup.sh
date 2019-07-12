61ad46409355115ec367892dae2c28f5800b8af0
Bonne <bonne.habekost@gmail.com>
2019-07-12 13:26:25 +0200

    first working version

--- run_setup.sh
+++ run_setup.sh
@@ -1,4 +1,16 @@
 # run setup for docker container
+
+docker images # show docker images
+docker container prune # clean up container
+docker image prune # clean up images
+CMD="Docker images cleaned"
+echo $CMD
+docker ps # show running docker images
+
+# start the docker image
+sh start_bigd.sh
+
+# run Ansible playbook
 ansible-playbook bigd_setup.yml -i hosts.ini
 
 
