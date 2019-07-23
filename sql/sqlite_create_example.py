dfc9aba1c77af5cb29adf1ad559c453c20a4a790
bhabekost <bonne.habekost@gmail.com>
2019-07-23 11:55:58 +0200

    creating a sqlite database works fine.

---
+++ sql/sqlite_create_example.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+
+# engine = create_engine("sqlite:///:memory", echo=True)
+# engine = create_engine("sqlite:///test.db", echo=True)
+db = create_engine("sqlite:///test.db", echo=False)
+
+metadata = MetaData(db)
+
+# create the database
+users = Table("users", metadata,
+              Column("user_id", Integer, primary_key=True),
+              Column("name", String(40)),
+              Column("age", Integer),
+              Column("password", String))
+users.create()
+
+# insert some users
+i = users.insert()
+i.execute(name="Mary", age=30, password="secret")
+
+
