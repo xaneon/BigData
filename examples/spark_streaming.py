09c38ab34f56fd2c6bde68ae134f47ab6d69c887
Bonne <bonne.habekost@gmail.com>
2019-09-24 14:34:36 +0200

    simple get tweets function

--- examples/spark_streaming.py
+++ examples/spark_streaming.py
@@ -13,4 +13,9 @@ my_auth = requests_oauthlib.OAuth1(CONSUMER_KEY,
 
 def get_tweets():
     url = "https://stream.twitter.com/1.1/statuses/filter.json"
-    # TODO
+    query_data = [("language", "de"), ("locations", "-130, -20, 100, 50"),
+                  ("track", "#")]
+    query_url = url + "?" + "&".join([str(t[0]) + "=" + str(t[1]) for t in query_data])
+    response = requests.get(query_url, auth=my_auth, stream=True)
+    print(query_url, response)
+    return response
